7da03d11c3f9eddd857bb08e02905408eb654197
kaiser1024 <kaiser.laszlo@gmail.com>
2026-04-16 10:32:04 +0300

    Add intake configuration to calcxregxm1xhuxeuxeux class

--- calgen.php
+++ calgen.php
@@ -11,7 +11,12 @@ if ( ! defined( 'ABSPATH') ) exit;
 
 class calcxregxm1xhuxeuxeux {
 
+            private $intakexeuxeu = [];
             private $lblxeuxeu = [];
+            private $lblxdependencexeuxeu = [];
+            private $lblxancestorxeuxeu = [];
+            private $lblxamcestorexistxeuxeu = [];
+            private $lblxamcestorvaluexeuxeu = [];
             private $lblxdvxeuxeu = [];
             private $lblsxdvxdsplxeuxeu = [];
             private $lblxnmxeuxeu = [];
@@ -21,9 +26,88 @@ class calcxregxm1xhuxeuxeux {
 
 	public function __construct() {
 
+		$intakexeuxeu = [
+
+			'intake1xeuxeu' => [
+				'wrapper1xdividxeuxeu' => 'divid',
+				'wrapper1xdisplayxeueu' => 'block/none',
+				'label1xlabelforxeuxeu' => 'labelfor',
+				'label1xlabeltextxeuxeu' => 'Label Text',
+				'label1xselectnamexeuxeu' => 'selectname',
+				'label1xselectidxeuxeu' => 'selectid',
+				'label1xoptionvaluesandnamesxeuxeu' => [
+						'label1xoptionvalue1xeuxeu' => 'Option 1',
+						'label1xoptionvalue2xeuxeu' => 'Option 2',
+						'label1xoptionvalue3xeuxeu' => 'Option 3',
+						],
+				'intake1xancestorxeuxeu' => 'ancestorselectname/none',
+				'intake1xreversorxeuxeu' => 'ancestoroptionvalue/blank',
+				'label1xerroridxeuxeu' => 'errorid',
+				'label1xerrortextxeuxeu' => 'Error Text',
+				'intake1xoutputintrotextxeuxeu' => 'Output Intro Text'/ none,
+				'intake1xoutputresulttextxeuxeu' => 'Output Result Text'/none',
+				'intake1xoutputresultspanxeuxeu' => 'outputresultspan/none',
+				'intake1xoutputnumericalxeuxeu' => 'yes/no',
+				'intake1xoutputtexttonumericaltransformxeuxeu' => 'yes/no',
+				'intake1xoutputnumericaltransfortablemxeuxeu' =>  [
+						'select1' => '1',
+						'select2' => '2',
+						'select3' => '3',
+						],
+				'intake1xoutputintermediaryvalue1xeuxeu' =>  [
+						'select1' => '1',
+						'select2' => '2',
+						'select3' => '3',
+						],
+				'intake1xoutputintermediaryvalue2xeuxeu' =>  [
+						'select1' => '1',
+						'select2' => '2',
+						'select3' => '3',
+						],
+
+				'intake1xoutputmultiplier1xeuxeu' =>  [
+						'select1' => '1',
+						'select2' => '2',
+						'select3' => '3',
+						],
+				'intake1xoutputmultiplier2xeuxeu' =>  [
+						'select1' => '1',
+						'select2' => '2',
+						'select3' => '3',
+						],
+				'intake1xoutputdivider1xeuxeu' =>  [
+						'select1' => '1',
+						'select2' => '2',
+						'select3' => '3',
+						],
+
+				'intake1xoutputdivider2xeuxeu' =>  [
+						'select1' => '1',
+						'select2' => '2',
+						'select3' => '3',
+						],
+				'intake1xinheritedvalue1xeuxeu' =>  => 'const',
+				'intake1xinheritedvalue2xeuxeu' =>  => 'const',
+				'intake1xinheritedvalue3xeuxeu' =>  => 'const',
+				'intake1xpassonvalue1xeuxeu' =>  => 'const',
+				'intake1xpassonvalue2xeuxeu' =>  => 'const',
+				'intake1xpassonvalue3xeuxeu' =>  => 'const',
+
+
+				'intake1xoutputintrospanxeuxeu' => 'outputintrospan/none',
+
+				],
+
+		];
+
+
             add_shortcode('calcxregxm1xhuxeuxeux', [$this, 'render']);
 
   	}
+
+
+        public function enqueue_scripts() {  }
+
 
 
 	public function render() {
@@ -32,10 +116,21 @@ class calcxregxm1xhuxeuxeux {
 
 			<form id="formxeuxeu">
 
+			<div id="ntkxwrpprxeuxeu" display: block/none>
+    				<label for="lblxeuxeu"><?php echo esc_html($this->labels['lblxeuxeu'])?></label>
+				<select name="lblxnmxeuxeu" id="lblxidxeuxeu"><option value="">Válassz...</option>
+        				<?php foreach ($this->lblxptnxeuxeu as $value => $label): ?>
+						<option value="<?php echo esc_attr($value); ?>">
+							<?php echo esc_html($label); ?>
+						</option>
+					<?php endforeach; ?>
+				</select>
+
 
 				?>
 
             		<script>
+
 			const lblxeuxeu = <?php echo wp_json_encode($this->lblxeuxeu); ?>;
 			const lblxdvxeuxeu = <?php echo wp_json_encode($this->lblxdvxeuxeu); ?>;
 			const lblsxdvxdsplxeuxeu = <?php echo wp_json_encode($this->lblsxdvxdsplxeuxeu); ?>;
@@ -43,6 +138,13 @@ class calcxregxm1xhuxeuxeux {
 			const lblxidxeuxeu = <?php echo wp_json_encode($this->lblxidxeuxeu); ?>;
 			const lblxptnxeuxeu = <?php echo wp_json_encode($this->lblxptnxeuxeu); ?>;
 			const lblxrrrxeuxeu = <?php echo wp_json_encode($this->lblxrrrxeuxeu); ?>;
+
+			document.addEventListener("DOMContentLoaded", () => {
+
+    				const calcbuttonxeuxeu = document.getElementById("calcbuttonxeuxeu");
+				const formxeuxeu = document.getElementById("formxeuxeu");
+
+
 
 			</script>
 
